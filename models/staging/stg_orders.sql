c2e2304f3f92d7f27062cf1d673c4fe44263c480
rotemsh18@gmail.com <rotemsh18@gmail.com>
2025-08-24 07:56:01 +0000

    added test of acceptable values for orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,2 +1,5 @@
 select *, ordersellingprice - ordercostprice as orderprofit
-from {{ref('raw_orders')}}
+from 
+{{ref('raw_orders')}} as raw_orders
+left join {{ ref('raw_customer') }} as raw_customer
+on raw_orders.customerid = raw_customer.customerid
